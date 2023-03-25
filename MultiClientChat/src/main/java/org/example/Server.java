5770f2c3700f409baea8567353f3e117af30306c
AlexanderExp <asfrolov_2@edu.hse.ru>
2023-03-25 16:08:46 +0300

    Add files via upload

---
+++ MultiClientChat/src/main/java/org/example/Server.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.io.*;
+import java.util.*;
+import java.net.*;
+
+public class Server {
+
+    static Vector<ClientHandler> ar = new Vector<>();
+
+    static int i = 0;
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(1234);
+
+        Socket s;
+        List<String> names = new ArrayList<>();
+        while (true) {
+            s = ss.accept();
+            System.out.println("New client request received : " + s);
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            System.out.println("Creating a new handler for this client...");
+            Scanner scn = new Scanner(System.in);
+            String name = scn.nextLine();
+            names.add(name);
+            ClientHandler mtch = new ClientHandler(s, "client " + name, dis, dos);
+            Thread t = new Thread(mtch);
+            System.out.println("Adding this client to active client list");
+            if (names.contains(name)) {
+                ar.add(mtch);
+                t.start();
+                names.add(name);
+            }
+        }
+    }
+}
